3c18962a195cdec6c8df9e2eed62a24b34c3bf6b
Giulia Raffaella De Luca <101675809+giurdeluca@users.noreply.github.com>
2025-05-04 16:29:44 +0200

    fix indent in main.py

--- EmphysemaSeg/main.py
+++ EmphysemaSeg/main.py
@@ -32,73 +32,71 @@ from EmphysemaSeg.utils import clip_mask_overlay, nearest_label_filling, get_lar
 
 
 def infer(config, images, emp_seg=False, vis=False):
-        print(emp_seg, vis)
-        device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-        # Create output directories if they don't exist
-        os.makedirs(config["seg_dir"], exist_ok=True)
+    print(emp_seg, vis)
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    # Create output directories if they don't exist
+    os.makedirs(config["seg_dir"], exist_ok=True)
 
-	# Use the provided file list instead of glob
-        infer_loader = infer_dataloader(config, images)
+    # Use the provided file list instead of glob
+    infer_loader = infer_dataloader(config, images)
 
-        model = UNet(
-                spatial_dims=3,
-                in_channels=1,
-                out_channels=6,
-                channels=(32, 64, 128, 256, 512),
-                strides=(2, 2, 2, 2),
-                num_res_units=2,
-                norm=Norm.BATCH,
-        ).to(device)
-        model.load_state_dict(torch.load(config['model_path']))
+    model = UNet(
+        spatial_dims=3,
+        in_channels=1,
+        out_channels=6,
+        channels=(32, 64, 128, 256, 512),
+        strides=(2, 2, 2, 2),
+        num_res_units=2,
+        norm=Norm.BATCH,
+    ).to(device)
+    model.load_state_dict(torch.load(config['model_path']))
 
-        model.eval()
-        with torch.no_grad():
-                for batch in tqdm(infer_loader):
-                        data, image_path = batch["image"].to(
-                                device), batch["image_path"][0]
-                        fname = os.path.basename(image_path).split(".nii.gz")[0]
+    model.eval()
+    with torch.no_grad():
+        for batch in tqdm(infer_loader):
+            data, image_path = batch["image"].to(device), batch["image_path"][0]
+            fname = os.path.basename(image_path).split(".nii.gz")[0]
 
-                        raw_nii = nib.load(image_path)
-                        axcodes = nib.orientations.aff2axcodes(raw_nii.affine)
-                        axcodes = ''.join(axcodes)
-                        pixdim = raw_nii.header.get_zooms()
-                        spatial_size = raw_nii.shape
+            raw_nii = nib.load(image_path)
+            axcodes = nib.orientations.aff2axcodes(raw_nii.affine)
+            axcodes = ''.join(axcodes)
+            pixdim = raw_nii.header.get_zooms()
+            spatial_size = raw_nii.shape
 
-                        post_pred_transforms = Compose([
-                                EnsureType(),
-                                AsDiscrete(argmax=True),
-                                Orientation(axcodes=axcodes),
-                                Spacing(pixdim=pixdim, mode="nearest"),
-                                Resize(spatial_size=spatial_size, mode="nearest"),
-                        ])
-                        pred = sliding_window_inference(data, (96, 96, 96), 4, model)
-                        pred = post_pred_transforms(pred[0].detach().cpu().numpy())
-                        lobe_map = nearest_label_filling(pred[0], get_largest_cc(pred[0]))
+            post_pred_transforms = Compose([
+                EnsureType(),
+                AsDiscrete(argmax=True),
+                Orientation(axcodes=axcodes),
+                Spacing(pixdim=pixdim, mode="nearest"),
+                Resize(spatial_size=spatial_size, mode="nearest"),
+            ])
+            pred = sliding_window_inference(data, (96, 96, 96), 4, model)
+            pred = post_pred_transforms(pred[0].detach().cpu().numpy())
+            lobe_map = nearest_label_filling(pred[0], get_largest_cc(pred[0]))
 
-                        if emp_seg:
-                                # emp class encoded in second channel
-                                lung_mask = np.where(lobe_map, 1, 0)
-                                img = raw_nii.get_fdata()
-                                emp = np.where(img < -950, 1, 0)
-                                emp = np.multiply(lung_mask, emp)
-                                label_map = np.array([lobe_map, emp])
-                        else:
-                                label_map = lobe_map
+            if emp_seg:
+                # emp class encoded in second channel
+                lung_mask = np.where(lobe_map, 1, 0)
+                img = raw_nii.get_fdata()
+                emp = np.where(img < -950, 1, 0)
+                emp = np.multiply(lung_mask, emp)
+                label_map = np.array([lobe_map, emp])
+            else:
+                label_map = lobe_map
 
-                        label_map_nii = nib.Nifti1Image(label_map, header=raw_nii.header, affine=raw_nii.affine)
-                        nib.save(label_map_nii, os.path.join(config["seg_dir"], f"{fname}_lobes.nii.gz"))
+            label_map_nii = nib.Nifti1Image(label_map, header=raw_nii.header, affine=raw_nii.affine)
+            nib.save(label_map_nii, os.path.join(config["seg_dir"], f"{fname}_lobes.nii.gz"))
 
-                        if vis:
-				os.makedirs(config["clip_dir"], exist_ok=True)
-                                if emp_seg:
-                                        # labal map encodes emphysema as 7
-                                        label_map = np.where(emp, 7, lobe_map)
-                                for plane in ["coronal", "axial", "sagittal"]:
-                                        clip_mask_overlay(raw_nii.get_fdata(), label_map,
-                                                                          os.path.join(config["clip_dir"], f"{fname}_{plane}.png"),
-                                                                          clip_plane=plane,
-                                                                          img_vrange=(-1000, 0))
-
+            if vis:
+                os.makedirs(config["clip_dir"], exist_ok=True)
+                if emp_seg:
+                    # label map encodes emphysema as 7
+                    label_map = np.where(emp, 7, lobe_map)
+                for plane in ["coronal", "axial", "sagittal"]:
+                    clip_mask_overlay(raw_nii.get_fdata(), label_map,
+                                      os.path.join(config["clip_dir"], f"{fname}_{plane}.png"),
+                                      clip_plane=plane,
+                                      img_vrange=(-1000, 0))
 
 def infer_dataloader(config, image_files):
         test_transforms = Compose([
